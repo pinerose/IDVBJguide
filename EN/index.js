a71957f8b44cd82ae3a178518631ba02a7b17d26
pinerose <60974032+pinerose@users.noreply.github.com>
2020-04-02 01:39:25 +0900

    IDVBJ guide English version 1(incomplete)

---
+++ EN/index.js
@@ -0,0 +1,112 @@
+function Hide1(){
+	document.querySelector('.HidePage1 ').style.display = 'none';
+	document.querySelector('.HidePage1 ul').style.display = 'none';
+	document.querySelector('.page1').style.display = 'block';
+}
+function Hide2(){
+	document.querySelector('.HidePage2').style.display = 'none';
+	document.querySelector('.HidePage2 ul').style.display = 'none';
+	document.querySelector('.page2').style.display = 'block';
+}
+function Hide3(){
+	document.querySelector('.HidePage3').style.display = 'none';
+	document.querySelector('.HidePage3 ul').style.display = 'none';
+	document.querySelector('.page3').style.display = 'block';
+}
+function Hide4(){
+	document.querySelector('.HidePage4').style.display = 'none';
+	document.querySelector('.HidePage4 ul').style.display = 'none';
+	document.querySelector('.page4').style.display = 'block';
+}
+function Hide5(){
+	document.querySelector('.HidePage5').style.display = 'none';
+	document.querySelector('.HidePage5 ul').style.display = 'none';
+	document.querySelector('.page5').style.display = 'block';
+}
+
+function Show1(){
+	document.querySelector('.HidePage1').style.display = 'block';
+	document.querySelector('.HidePage1 ul').style.display = 'block';
+	document.querySelector('.page1').style.display = 'none';
+	document.querySelector('.HidePage2').style.display = 'none';
+	document.querySelector('.HidePage2 ul').style.display = 'none';
+	document.querySelector('.page2').style.display = 'block';
+	document.querySelector('.HidePage3').style.display = 'none';
+	document.querySelector('.HidePage3 ul').style.display = 'none';
+	document.querySelector('.page3').style.display = 'block';
+	document.querySelector('.HidePage4').style.display = 'none';
+	document.querySelector('.HidePage4 ul').style.display = 'none';
+	document.querySelector('.page4').style.display = 'block';
+	document.querySelector('.HidePage5').style.display = 'none';
+	document.querySelector('.HidePage5 ul').style.display = 'none';
+	document.querySelector('.page5').style.display = 'block';
+
+}
+function Show2(){
+	document.querySelector('.HidePage2').style.display = 'block';
+	document.querySelector('.HidePage2 ul').style.display = 'block';
+	document.querySelector('.page2').style.display = 'none';
+	document.querySelector('.HidePage1 ').style.display = 'none';
+	document.querySelector('.HidePage1 ul').style.display = 'none';
+	document.querySelector('.page1').style.display = 'block';
+	document.querySelector('.HidePage3').style.display = 'none';
+	document.querySelector('.HidePage3 ul').style.display = 'none';
+	document.querySelector('.page3').style.display = 'block';
+	document.querySelector('.HidePage4').style.display = 'none';
+	document.querySelector('.HidePage4 ul').style.display = 'none';
+	document.querySelector('.page4').style.display = 'block';
+	document.querySelector('.HidePage5').style.display = 'none';
+	document.querySelector('.HidePage5 ul').style.display = 'none';
+	document.querySelector('.page5').style.display = 'block';
+}
+function Show3(){
+	document.querySelector('.HidePage3').style.display = 'block';
+	document.querySelector('.HidePage3 ul').style.display = 'block';
+	document.querySelector('.page3').style.display = 'none';
+	document.querySelector('.HidePage1 ').style.display = 'none';
+	document.querySelector('.HidePage1 ul').style.display = 'none';
+	document.querySelector('.page1').style.display = 'block';
+	document.querySelector('.HidePage2').style.display = 'none';
+	document.querySelector('.HidePage2 ul').style.display = 'none';
+	document.querySelector('.page2').style.display = 'block';
+	document.querySelector('.HidePage4').style.display = 'none';
+	document.querySelector('.HidePage4 ul').style.display = 'none';
+	document.querySelector('.page4').style.display = 'block';
+	document.querySelector('.HidePage5').style.display = 'none';
+	document.querySelector('.HidePage5 ul').style.display = 'none';
+	document.querySelector('.page5').style.display = 'block';
+}
+function Show4(){
+	document.querySelector('.HidePage4').style.display = 'block';
+	document.querySelector('.HidePage4 ul').style.display = 'block';
+	document.querySelector('.page4').style.display = 'none';
+	document.querySelector('.HidePage1 ').style.display = 'none';
+	document.querySelector('.HidePage1 ul').style.display = 'none';
+	document.querySelector('.page1').style.display = 'block';
+	document.querySelector('.HidePage2').style.display = 'none';
+	document.querySelector('.HidePage2 ul').style.display = 'none';
+	document.querySelector('.page2').style.display = 'block';
+	document.querySelector('.HidePage3').style.display = 'none';
+	document.querySelector('.HidePage3 ul').style.display = 'none';
+	document.querySelector('.page3').style.display = 'block';
+	document.querySelector('.HidePage5').style.display = 'none';
+	document.querySelector('.HidePage5 ul').style.display = 'none';
+	document.querySelector('.page5').style.display = 'block';
+}
+function Show5(){
+	document.querySelector('.HidePage5').style.display = 'block';
+	document.querySelector('.HidePage5 ul').style.display = 'block';
+	document.querySelector('.page5').style.display = 'none';
+	document.querySelector('.HidePage1 ').style.display = 'none';
+	document.querySelector('.HidePage1 ul').style.display = 'none';
+	document.querySelector('.page1').style.display = 'block';
+	document.querySelector('.HidePage2').style.display = 'none';
+	document.querySelector('.HidePage2 ul').style.display = 'none';
+	document.querySelector('.page2').style.display = 'block';
+	document.querySelector('.HidePage3').style.display = 'none';
+	document.querySelector('.HidePage3 ul').style.display = 'none';
+	document.querySelector('.page3').style.display = 'block';
+	document.querySelector('.HidePage4').style.display = 'none';
+	document.querySelector('.HidePage4 ul').style.display = 'none';
+	document.querySelector('.page4').style.display = 'block';
+}
